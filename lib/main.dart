ea054b7dc4ba4026079b24ea15bd87e185740157
Mr Xcode <mrxcode.dev@gmail.com>
2025-11-03 01:21:05 +0630

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     GetMaterialApp(
       title: "Application",
